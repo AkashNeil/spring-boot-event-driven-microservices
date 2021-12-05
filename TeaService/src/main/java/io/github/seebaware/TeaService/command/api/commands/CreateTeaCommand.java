7604af1d65956a7fb4ac549e1d57e8e773576507
Neil <akashneil@live.com>
2021-12-05 19:52:08 +0400

    tea setvice

--- TeaService/src/main/java/io/github/seebaware/TeaService/command/api/commands/CreateTeaCommand.java
+++ TeaService/src/main/java/io/github/seebaware/TeaService/command/api/commands/CreateTeaCommand.java
@@ -1,8 +1,20 @@
 package io.github.seebaware.TeaService.command.api.commands;
 
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
 public class CreateTeaCommand {
 
-    
+    @TargetAggregateIdentifier
+    private String teaId;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
 
 }
 
