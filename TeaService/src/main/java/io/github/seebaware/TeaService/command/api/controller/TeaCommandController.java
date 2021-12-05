ed11560b7c05801ad1cfea4d3001b9e7bf87be68
Neil <akashneil@live.com>
2021-12-05 19:53:52 +0400

    tea setvice

--- TeaService/src/main/java/io/github/seebaware/TeaService/command/api/controller/TeaCommandController.java
+++ TeaService/src/main/java/io/github/seebaware/TeaService/command/api/controller/TeaCommandController.java
@@ -1,11 +1,14 @@
 package io.github.seebaware.TeaService.command.api.controller;
 
+import io.github.seebaware.TeaService.command.api.commands.CreateTeaCommand;
 import io.github.seebaware.TeaService.command.api.model.TeaRestModel;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/teas")
@@ -19,7 +22,16 @@ public class TeaCommandController {
 
     @PostMapping
     public String addTea(@RequestBody TeaRestModel teaRestModel) {
-        return "Tea added";
+
+        CreateTeaCommand createTeaCommand = CreateTeaCommand.builder()
+                .teaId(UUID.randomUUID().toString())
+                .name(teaRestModel.getName())
+                .price(teaRestModel.getPrice())
+                .quantity(teaRestModel.getQuantity())
+                .build();
+        
+        return commandGateway.sendAndWait(createTeaCommand);
+
     }
 
 }
